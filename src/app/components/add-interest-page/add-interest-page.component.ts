07be3de864856c6026f2d340765a36cfac2d81f3
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-07 11:49:22 -0800

    feat: interet page functionality done🚀🔥

--- src/app/components/add-interest-page/add-interest-page.component.ts
+++ src/app/components/add-interest-page/add-interest-page.component.ts
@@ -1,10 +1,11 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-interest-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass, NgFor],
   templateUrl: './add-interest-page.component.html',
   styleUrl: './add-interest-page.component.css',
 })
@@ -12,9 +13,37 @@ export class AddInterestPageComponent {
   backgroundImage = '../assets/interest.png';
 
   interestArray = [
-    { name: 'Social Interaction' },
-    { name: 'Personal development' },
-    { name: 'Entertainment and Fun' },
-    { name: 'Rewards and Recognition' },
+    {
+      name: 'Social Interaction',
+      imgPath: '../assets/add.png',
+      selectedImgPath: '../assets/add-1.png',
+    },
+    {
+      name: 'Personal development',
+      imgPath: '../assets/add.png',
+      selectedImgPath: '../assets/add-1.png',
+    },
+    {
+      name: 'Entertainment and Fun',
+      imgPath: '../assets/add.png',
+      selectedImgPath: '../assets/add-1.png',
+    },
+    {
+      name: 'Rewards and Recognition',
+      imgPath: '../assets/add.png',
+      selectedImgPath: '../assets/add-1.png',
+    },
   ];
+
+  selectedIndex: number | null = null;
+
+  selectInterest(index: number): void {
+    if (this.selectedIndex === index) {
+      // Deselect the button if it's already selected
+      this.selectedIndex = null;
+    } else {
+      // Select the clicked button
+      this.selectedIndex = index;
+    }
+  }
 }
