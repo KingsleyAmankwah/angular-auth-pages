580d9c3d2a799a32bda1073a3bfc602bfd2d597d
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-08 11:13:48 -0800

    feat/to be worked on next week

--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,32 +1,30 @@
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+// import {
+//   FormControl,
+//   FormGroup,
+//   ReactiveFormsModule,
+//   Validators,
+// } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { UserService } from '../../services/user.service';
+// import { UserService } from '../../services/user.service';
 import { NgIf } from '@angular/common';
-import { userFormData } from '../../types';
+// import { userFormData } from '../../types';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, NgIf],
+  imports: [RouterLink, NgIf],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
-  constructor(private userService: UserService) {}
+  // constructor(private userService: UserService) {}
   // userService = inject(UserService);
-
-  registerForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
-  });
-
+  // registerForm = new FormGroup({
+  //   name: new FormControl('', Validators.required),
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   password: new FormControl('', Validators.required),
+  // });
   // handleRegisterClick() {
   //   if (this.registerForm.valid) {
   //     const { name, email, password } = this.registerForm.value;
@@ -37,29 +35,26 @@ export class RegisterPageComponent {
   //     );
   //   }
   // }
-
-  handleRegisterClick() {
-    if (this.registerForm.valid) {
-      const { name, email, password } = this.registerForm.value;
-
-      const formData: userFormData = {
-        name: name!,
-        email: email!,
-        password: password!,
-      };
-
-      this.userService.registerUser(formData).subscribe(
-        (response) => {
-          console.log('User registered successfully:', response);
-        },
-        (error) => {
-          console.error('Error registering user:', error);
-        }
-      );
-    } else {
-      console.error(
-        'Form is invalid. Please check error messages below each field.'
-      );
-    }
-  }
+  // handleRegisterClick() {
+  //   if (this.registerForm.valid) {
+  //     const { name, email, password } = this.registerForm.value;
+  //     const formData: userFormData = {
+  //       name: name!,
+  //       email: email!,
+  //       password: password!,
+  //     };
+  //     this.userService.registerUser(formData).subscribe(
+  //       (response) => {
+  //         console.log('User registered successfully:', response);
+  //       },
+  //       (error) => {
+  //         console.error('Error registering user:', error);
+  //       }
+  //     );
+  //   } else {
+  //     console.error(
+  //       'Form is invalid. Please check error messages below each field.'
+  //     );
+  //   }
+  // }
 }
